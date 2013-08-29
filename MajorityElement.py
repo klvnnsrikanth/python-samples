bbce31a5e06a2c5d011e0cb83c9ba43814d3325a
klvnnsrikanth <klvnnsrikanth@gmail.com>
2013-08-29 18:24:06 +0530

    Majority element code

--- MajorityElement.py
+++ MajorityElement.py
@@ -2,6 +2,17 @@
 Find the majority element which occurs more than n/2 times in an array of n size, which contains duplicate elements in minimum time and space complexity.
 """
 
+def validate(array, majority):
+	length = len(array)
+	count = 0
+	for elem in array:
+		if elem == majority:
+			count += 1
+	if count > (length/2):
+		print majority
+	else:
+		print "No Majority Element"
+				
 def majority_element(array):
 	majority = array[0]
 	count = 0
@@ -13,7 +24,8 @@ def majority_element(array):
 			if count == 0:
 				majority = elem
 				count += 1
-	print majority
+	validate(array,  majority)
 
 
-majority_element([1,3,4,1,1,7,1])	
+majority_element([1,3,4,1,1,7,1])
+majority_element([23,11,3,44,1,11,7,11])
